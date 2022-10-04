93f5186ab9c2bc1d28c6cebec591ce701d57b002
Pc Patrick Casa <patrickalvares@outlook.com>
2022-10-03 21:47:21 -0300

    images are now assets kind

--- lib/screens/initial_screen.dart
+++ lib/screens/initial_screen.dart
@@ -21,24 +21,14 @@ class _InitialScreenState extends State<InitialScreen> {
         opacity: opacidade ? 1 : 0,
         duration: const Duration(milliseconds: 1000),
         child: ListView(children: const [
-          Task(
-              'Aprender Flutter',
-              'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-              4),
-          Task(
-              'Andar de bike',
-              'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg',
-              2),
-          Task(
-              'Meditar',
-              'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
-              5),
-          Task('Jogar video-game',
-              'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg', 1),
-          Task(
-              'Ler',
-              'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',
-              2),
+          Task('Aprender Flutter', 'assets/images/dash.png', 4),
+          Task('Andar de bike', 'assets/images/bike.webp', 2),
+          Task('Meditar', 'assets/images/meditar.jpeg', 5),
+          Task('Jogar video-game', 'assets/images/jogar.jpg', 1),
+          Task('Ler', 'assets/images/livro.jpg', 2),
+          SizedBox(
+            height: 80,
+          )
         ]),
       ),
       floatingActionButton: FloatingActionButton(
